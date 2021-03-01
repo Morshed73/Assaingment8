1cc28386a4daedfcd7028bc0080866466137a58a
Morshed73 <morshed73720@gmail.com>
2021-03-02 01:26:15 +0600

    footer and others added

--- js/main.js
+++ js/main.js
@@ -69,5 +69,24 @@ $(document).ready(function(){
         }
     });
 
+    /*--- Team Corousle ---*/
+    $('.team-corousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
 
 });
